643dc8e7bd797fe072d5c772f1ffc395dbd948a7
Deepak Prasad <121165383+prasaddeep@users.noreply.github.com>
2024-08-24 02:56:35 +0000

    suummary2

--- datatypes-summary.js
+++ datatypes-summary.js
@@ -4,3 +4,19 @@
 
 // Reference (non-primitive):- Array, Object, Function
 
+const score = 10;
+
+const scoreValue = 10.0;
+
+const isLoggedIn = false;
+
+const outsideTemp = null;
+
+let userEmail;
+
+const id = symbol("123");
+
+const anotherID = symbol("123")
+
+const bigNumber = 633323457536764364738n
+
