1c9e99283876c4f2450a94c29ac2268c8f9fc7aa
Deepak Prasad <121165383+prasaddeep@users.noreply.github.com>
2024-08-26 00:31:20 +0000

    string_method

---
+++ basic/string.js
@@ -0,0 +1,28 @@
+const name = "deepak Prasad";
+
+const repoCount = 50;
+
+// console.log(name + repoCount + "value");
+
+// console.log(`Hellow my name is ${name} and my repo count is ${repoCount} `);
+
+const gameName = new String(`dprof-jtrst`)
+
+// console.log(gameName[0]);
+// console.log(gameName._proto_);
+
+// console.log(gameName.length);
+// console.log(gameName.toUpperCase());
+// console.log(gameName.charAt(3));
+// console.log(gameName.indexOf('j'));
+
+const newString = gameName.substring(0,4);
+console.log(newString);
+
+
+const anotherString = gameName.slice(-8,4);
+console.log(anotherString);
+
+const newStringOne = "   Deepak   ";
+console.log(newStringOne);
+console.log(newStringOne.trim());
