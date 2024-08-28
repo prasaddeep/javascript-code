56871e859573d1a95bc108888a5263baa5bba651
Deepak Prasad <121165383+prasaddeep@users.noreply.github.com>
2024-08-28 02:31:04 +0000

    date_function

---
+++ basic/date.js
@@ -0,0 +1,13 @@
+let myDate = new Date()
+
+// console.log(myDate.toDateString());
+// console.log(myDate.toLocaleDateString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toLocaleTimeString());
+// console.log(myDate.toDateString());
+
+console.log(typeof myDate);
+
+let myCreateDate = new Date(2023, 0, 28);
+console.log(myCreateDate);
+console.log(myCreateDate.toDateString());
