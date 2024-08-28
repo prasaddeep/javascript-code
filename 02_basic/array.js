5ae43a8c2b7a100d4dcab9b637b1f3b190aea103
Deepak Prasad <121165383+prasaddeep@users.noreply.github.com>
2024-08-28 02:53:25 +0000

    arrays_mthods

---
+++ 02_basic/array.js
@@ -0,0 +1,16 @@
+// array 
+
+const array = [0,1,2,3,4,5]
+const heros = ['sktiman','nagraj']
+
+// const myArr = new Aray(0,1,2,3,4,5)
+
+console.log(array[1]);
+
+// Array methods 
+
+array.push(6)
+array.push(7)
+array.pop()
+
+console.log(array);
