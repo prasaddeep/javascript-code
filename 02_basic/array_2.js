5d7d2d2aacc48ee2c3fdd1df82f96e2843098ead
Deepak Prasad <121165383+prasaddeep@users.noreply.github.com>
2024-08-29 02:43:37 +0000

    array_continue

---
+++ 02_basic/array_2.js
@@ -0,0 +1,31 @@
+const maeval_heros = ["thore", "Ironmen", "spidermen"]
+const dc_heros = ["supermen","flash","batmen"]
+
+// maeval_heros.push(dc_heros)
+
+// console.log(maeval_heros);
+
+// const allHeros = maeval_heros.concat(dc_heros)
+
+// console.log(allHeros);
+
+// const all_new_heros = [...maeval_heros, ...dc_heros]
+// console.log(all_new_heros);
+
+
+// const another_array = [1,2,3,[4,5,6],7,[6,7[4,5]]]
+
+
+// const real_another_array = another_array.flat()
+// console.log(real_another_array);
+
+console.log(Array.isArray("Deepak"))
+console.log(Array.from("Deepak"))
+console.log(Array.isArray({name:"Deepak"}))
+
+let score1 = 100
+let score2 = 200
+let score3 = 300
+
+console.log(Array.of (score1,score2,score3));
+
